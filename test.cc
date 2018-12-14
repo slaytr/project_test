01b11e970f67b5f45e50193485700a8db46fe752
Bill Li <bill.li@nyriad.com>
2018-12-15 12:20:05 +1300

    Syntax Testing

---
+++ test.cc
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "test.h"
+
+int main()
+{
+    TestParam box;
+    box.numberCheck();
+    box.numberCheck(1);
+    return 0;
+}
---
+++ test.h
@@ -0,0 +1,8 @@
+class TestParam
+{
+public:
+    void numberCheck(bool integer = false, int apple = 0){
+        printf("%d\n", (int)integer);
+        printf("apple %d\n", apple);
+    }
+};
